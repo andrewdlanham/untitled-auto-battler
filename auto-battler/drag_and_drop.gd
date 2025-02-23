271e62cf7c85acdff5dd5506322706550a686ea0
Andrew Lanham <andrewdlanham@gmail.com>
2025-02-23 18:34:28 -0500

    Refactored drag and drop script

--- auto-battler/drag_and_drop.gd
+++ auto-battler/drag_and_drop.gd
@@ -1,45 +1,70 @@
 extends Node3D
 
-const RAY_LENGTH = 1000
+const RAY_LENGTH = 100
+const UNIT_MASK = 1
+const FLOOR_MASK = 2
+const UNIT_FLOOR_MASK = 3	# Enables both layers 1 and 2
 
 @onready var camera: Camera3D = %Camera3D
 
 var is_dragging
 var dragged_object
+var dragged_object_collision_shape
+var raycast_collision_mask = UNIT_MASK
 
-func _process(delta):
+func _process(_delta):
 	if is_dragging: update_dragged_object_position()
 
 func _input(event):
 	if Input.is_action_just_pressed("LeftClick"):
-		print("Left click detected")
-		var ray_intersect_dict = get_raycast_intersect()
-		if !ray_intersect_dict.is_empty():
-			# "../." is the path of the Area3D's parent Node3D node
-			dragged_object = ray_intersect_dict["collider"].get_node("../.")
+		print("Left click")
+		if is_dragging: return
+		raycast_collision_mask = UNIT_MASK
+		var raycast_collision_info = get_raycast_collision_info()
+		print(raycast_collision_info)
+		if !raycast_collision_info.is_empty():
+			dragged_object = raycast_collision_info["collider"]
+			print("Dragging: " + dragged_object.get_parent().name)
+			dragged_object_collision_shape = dragged_object.find_child("CollisionShape3D")
+			dragged_object_collision_shape.set_disabled(true)
+			raycast_collision_mask = FLOOR_MASK
 			is_dragging = true
+			
 	elif Input.is_action_just_released("LeftClick"):
 		print("Left click released")
-		dragged_object = null
-		is_dragging = false
+		if is_dragging: finish_object_drag()
 
  
-func get_raycast_intersect():
-	# TODO : Rename / refactor this function
+func get_raycast_collision_info():
+	
 	var space_state = get_world_3d().direct_space_state
 	var mouse_position = get_viewport().get_mouse_position()
-	var origin = camera.project_ray_origin(mouse_position)
-	var end = origin + camera.project_ray_normal(mouse_position) * RAY_LENGTH
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
 
-	return space_state.intersect_ray(query)
+	var ray_start_point = camera.project_ray_origin(mouse_position)
+	var ray_end_point = ray_start_point + camera.project_ray_normal(mouse_position) * RAY_LENGTH
+	var ray_query = PhysicsRayQueryParameters3D.create(ray_start_point, ray_end_point)
+	ray_query.collide_with_areas = true
+	ray_query.collide_with_bodies = true
+	ray_query.collision_mask = raycast_collision_mask
+	
+	var intersect_ray_result = space_state.intersect_ray(ray_query)
+	
+	return intersect_ray_result
 	
 func update_dragged_object_position():
-	var updated_position = get_raycast_intersect().position
-	if updated_position:
+	var raycast_collision_info = get_raycast_collision_info()
+	if raycast_collision_info.is_empty(): return
+	if raycast_collision_info["position"]:
+		var updated_position = raycast_collision_info["position"]
 		dragged_object.global_position = Vector3(
 			updated_position.x,
 			dragged_object.global_position.y,
 			updated_position.z
 		)
+
+func finish_object_drag():
+	dragged_object_collision_shape.set_disabled(false)
+	dragged_object = null
+	dragged_object_collision_shape = null
+	raycast_collision_mask = UNIT_MASK
+	is_dragging = false
--- auto-battler/unit.gd
+++ auto-battler/unit.gd
@@ -3,12 +3,11 @@ extends Node3D
 @export var unit_name : String
 @export var health : float = 100.00
 @export var cost : int = 1
-@onready var health_label: Label3D = $HealthLabel
-
+@onready var health_label: Label3D = $Area3D/HealthLabel
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health_label.text = str(health)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
