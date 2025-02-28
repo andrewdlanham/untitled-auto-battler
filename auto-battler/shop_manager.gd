a86a7b4934e316ac165c156774c7350d90258033
Andrew Lanham <andrewdlanham@gmail.com>
2025-02-28 07:22:49 -0500

    Started working on shop logic

---
+++ auto-battler/shop_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_roll_shop_button_pressed() -> void:
+	print("Rolling shop...")
+	# TODO: Add shop roll logic
