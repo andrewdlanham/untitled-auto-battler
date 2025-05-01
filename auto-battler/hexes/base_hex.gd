145b254789226af9c41bbb93a32c85ce604406c8
Andrew Lanham <andrewdlanham@gmail.com>
2025-04-30 21:47:44 -0400

    refactor: add base hex scene for inheritance

--- auto-battler/hexes/base_hex.gd
+++ auto-battler/hexes/base_hex.gd
@@ -2,11 +2,14 @@ extends Node3D
 
 class_name Hex
 
-@onready var snap_point: Node3D = $"Snap Point"
+@onready var snap_point: Node3D = $SnapPoint
+@onready var hex_mesh: MeshInstance3D = $HexMesh
+
 @export var hex_id: String
 @export var hex_type: String
 @export var unit_on_hex: Unit
 @export var neighbors: Array[Node3D]
+
 
 func free_unit_on_hex() -> void:
 	if unit_on_hex != null: unit_on_hex.queue_free()
