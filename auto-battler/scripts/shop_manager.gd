6f1d17a8a1ad523c1e5eedb34072e0c97aeca96d
Andrew Lanham <andrewdlanham@gmail.com>
2025-05-02 15:13:34 -0400

    refactor: extract create_random_unit() from existing logic

--- auto-battler/scripts/shop_manager.gd
+++ auto-battler/scripts/shop_manager.gd
@@ -8,11 +8,11 @@ signal shop_roll_requested
 
 func _ready() -> void:
 	_connect_signals()
-	
+
+#region -- Signal Handlers --
 func _connect_signals() -> void:
 	%GoldManager.shop_roll_approved.connect(_on_roll_shop_approved)
 
-#region -- Signal Handlers --
 func _on_roll_shop_pressed() -> void:
 	shop_roll_requested.emit()
 
@@ -22,12 +22,14 @@ func _on_roll_shop_approved() -> void:
 		# Remove previous unit if needed
 		if shop_hex.unit_on_hex != null:
 			shop_hex.unit_on_hex.remove_self()
-		# Add new random unit
-		var rng = RandomNumberGenerator.new()
-		var random_unit_index = rng.randi_range(0, possible_units.size() - 1)
-		var new_unit = possible_units[random_unit_index].instantiate()
-		new_unit.position = shop_hex.snap_point.global_position
-		shop_hex.unit_on_hex = new_unit
-		new_unit.current_hex = shop_hex
+
+		var new_unit = create_random_unit()
+		new_unit.try_connect_to_hex(shop_hex)
 		%ShopUnits.add_child(new_unit)
 #endregion
+
+func create_random_unit() -> Unit:
+	var rng = RandomNumberGenerator.new()
+	var random_unit_index = rng.randi_range(0, possible_units.size() - 1)
+	var random_unit = possible_units[random_unit_index].instantiate()
+	return random_unit
