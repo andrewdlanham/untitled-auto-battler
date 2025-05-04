2a8a08162eed0872ff485b659660eb9c69488e00
Andrew Lanham <andrewdlanham@gmail.com>
2025-05-03 22:00:47 -0400

    feat: add function for connecting hexes to their neighboring hexes

--- auto-battler/hexes/base_hex.gd
+++ auto-battler/hexes/base_hex.gd
@@ -10,6 +10,10 @@ class_name Hex
 @export var unit_on_hex: Unit
 @export var neighbors: Array[Node3D]
 
+func _ready() -> void:
+	if is_player_hex() or is_enemy_hex():
+		_connect_to_neighbor_hexes()
+
 func is_occupied() -> bool:
 	return unit_on_hex != null
 
@@ -21,3 +25,11 @@ func is_player_hex() -> bool:
 
 func is_enemy_hex() -> bool:
 	return hex_type == "ENEMY"
+
+func _connect_to_neighbor_hexes() -> void:
+	var possible_neighbor_hexes = get_tree().root.get_node("Game/Hexes/PlayerHexes").get_children() + get_tree().root.get_node("Game/Hexes/EnemyHexes").get_children()
+	for hex in possible_neighbor_hexes:
+		if hex.name == self.name: continue
+		var distance = self.position.distance_to(hex.global_position)
+		if distance < 3:	# 3 is a temporary value for the range to check
+			neighbors.append(hex)
