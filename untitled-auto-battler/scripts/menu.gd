8c0f396a8df3e93379f5f3bf64f04cbf1e1c1c0d
Andrew Lanham <andrewdlanham@gmail.com>
2025-06-13 17:14:58 -0400

    feat: update menu to show user's login email

--- untitled-auto-battler/scripts/menu.gd
+++ untitled-auto-battler/scripts/menu.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@onready var logged_in_as_label: Label = $LoggedInAsLabel
+
+func _ready() -> void:
+	logged_in_as_label.text = "Logged in as " + DataManager.user_email
+	if (DataManager.user_email.contains("guest")):
+		logged_in_as_label.text = "Logged in as Guest"
 func _on_play_game_button_pressed() -> void:
 	print("Starting game...")
 	GameManager.start_game()
