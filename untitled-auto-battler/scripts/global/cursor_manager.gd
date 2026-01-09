bd4ab1fc77316839796227e68803d8015a1a7eab
Andrew Lanham <andrewdlanham@gmail.com>
2026-01-08 20:20:20 -0500

    fix: add cursor hotspot

--- untitled-auto-battler/scripts/global/cursor_manager.gd
+++ untitled-auto-battler/scripts/global/cursor_manager.gd
@@ -2,12 +2,13 @@ extends Node
 
 var DEFAULT_CURSOR = load("res://assets/images/cursors/point.png")
 var CLOSED_HAND_CURSOR = load("res://assets/images/cursors/closed_hand.png")
+var CURSOR_HOTSPOT = Vector2(12, 12)
 
 func _ready() -> void:
-	Input.set_custom_mouse_cursor(DEFAULT_CURSOR, Input.CURSOR_ARROW)
+	Input.set_custom_mouse_cursor(DEFAULT_CURSOR, Input.CURSOR_ARROW, CURSOR_HOTSPOT)
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("LeftClick"):
-		Input.set_custom_mouse_cursor(CLOSED_HAND_CURSOR, Input.CURSOR_ARROW)
+		Input.set_custom_mouse_cursor(CLOSED_HAND_CURSOR, Input.CURSOR_ARROW, CURSOR_HOTSPOT)
 	elif Input.is_action_just_released("LeftClick"):
-		Input.set_custom_mouse_cursor(DEFAULT_CURSOR, Input.CURSOR_ARROW)
+		Input.set_custom_mouse_cursor(DEFAULT_CURSOR, Input.CURSOR_ARROW, CURSOR_HOTSPOT)
