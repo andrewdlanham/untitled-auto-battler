66b286ced9d0dd0a6b0d82a2ce9b5809a19efceb
Andrew Lanham <andrewdlanham@gmail.com>
2026-01-21 17:03:38 -0500

    refactor: clean up scripts

--- untitled-auto-battler/scripts/menu.gd
+++ untitled-auto-battler/scripts/menu.gd
@@ -1,25 +1,26 @@
 extends Node2D
 
-@onready var logged_in_as_label: Label = $LoggedInAsLabel
-@onready var patch_notes_label: Label = %PatchNotesLabel
-
 func _ready() -> void:
-	DataManager.leaderboard_received.connect(update_leaderboard_label)
+	DataManager.leaderboard_received.connect(_update_leaderboard_label)
 	DataManager.get_leaderboard()
-	logged_in_as_label.text = "Logged in as " + Auth.user_profile["display_name"]
+	%LoggedInAsLabel.text = "Logged in as " + Auth.user_profile["display_name"]
 
-func _on_play_game_button_pressed() -> void:
-	GameManager.start_game()
-
-func update_leaderboard_label(data: Array):
+func _update_leaderboard_label(data: Array):
 	var leaderboard_text = "LEADERBOARD\n\n"
 	for i in data.size():
 		var entry = data[i]
 		leaderboard_text += str(i + 1) + ". " + entry["display_name"].substr(0, 16) + " - " + str(int(entry["wins"])) + " W\n"
 	%LeaderboardLabel.text = leaderboard_text
 
+#region Buttons
+
+func _on_play_game_button_pressed() -> void:
+	GameManager.start_game()
+
 func _on_log_out_button_pressed() -> void:
 	DataManager.log_out_current_user()
 
 func _on_patch_notes_button_pressed() -> void:
-	patch_notes_label.visible = !patch_notes_label.visible
+	%PatchNotesLabel.visible = !%PatchNotesLabel.visible
+
+#endregion
--- untitled-auto-battler/scripts/prep_scene/prep_scene.gd
+++ untitled-auto-battler/scripts/prep_scene/prep_scene.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 const STARTING_GOLD: int = 10
-@onready var round_label: Label = %RoundLabel
 
 func _ready() -> void:
 	_connect_signals()
@@ -10,24 +9,46 @@ func _ready() -> void:
 	GameManager.construct_team(GameManager.bench_units, %BenchHexes.get_children(), %BenchUnits)
 	GameManager.construct_team(GameManager.shop_units, %ShopHexes.get_children(), %ShopUnits)
 
-	start_new_round()
+	_start_new_round()
 
-func start_new_round() -> void:
+func _connect_signals() -> void:
+
+	# Everything that should trigger _update_unit_count_label()
+	%DragDropManager.unit_placed.connect(_update_unit_count_label)
+	%MergeManager.unit_merge_success.connect(_update_unit_count_label)
+	%UnitStatsPopup.unit_sold.connect(_update_unit_count_label)
+	
+	# Connect signals for UI buttons
+	%CombatButton.pressed.connect(_on_start_combat_button_pressed)
+	%RerollButton.pressed.connect(%ShopManager.request_shop_roll)
+
+func _start_new_round() -> void:
 	GameManager.current_round += 1
 	%GoldManager.set_gold(STARTING_GOLD)
 	%ShopManager.roll_shop_units()
-	update_unit_count_label()
-	update_round_label()
+	_update_unit_count_label()
+	_update_round_label()
 
-func update_unit_count_label() -> void:
+func _update_unit_count_label() -> void:
 	await get_tree().process_frame		# Allows units time to leave scene before updating the unit count label
 	%UnitCountLabel.text = "Units: " + str(%PlayerUnits.get_children().size()) + " / " + str(GameManager.get_current_unit_cap())
 
+func _update_round_label() -> void:
+	%RoundLabel.text = "Round   " + str(GameManager.current_round)
+
+func _get_unit_info_array(parent_node: Node) -> Array:
+	var unit_info_array = []
+	for unit: Unit in parent_node.get_children():
+		unit_info_array.append(unit.get_info_dict())
+	return unit_info_array
+
+#region Buttons
+
 func _on_start_combat_button_pressed() -> void:
 
-	GameManager.player_units = get_unit_info_array(%PlayerUnits)
-	GameManager.bench_units = get_unit_info_array(%BenchUnits)
-	GameManager.shop_units = get_unit_info_array(%ShopUnits)
+	GameManager.player_units = _get_unit_info_array(%PlayerUnits)
+	GameManager.bench_units = _get_unit_info_array(%BenchUnits)
+	GameManager.shop_units = _get_unit_info_array(%ShopUnits)
 
 	if (GameManager.player_units == []):
 		SceneManager.switch_to_scene(SceneManager.COMBAT_SCENE_PATH)
@@ -37,26 +58,6 @@ func _on_start_combat_button_pressed() -> void:
 		await DataManager.team_stored_in_db
 		SceneManager.switch_to_scene(SceneManager.COMBAT_SCENE_PATH)
 		SoundManager.play_music("combat_scene_music")
-
-func _connect_signals() -> void:
-
-	# Everything that should trigger update_unit_count_label()
-	%DragDropManager.unit_placed.connect(update_unit_count_label)
-	%MergeManager.unit_merge_success.connect(update_unit_count_label)
-	%UnitStatsPopup.unit_sold.connect(update_unit_count_label)
-	
-	# Connect signals for UI buttons
-	%CombatButton.pressed.connect(_on_start_combat_button_pressed)
-	%RerollButton.pressed.connect(%ShopManager.request_shop_roll)
-
-func update_round_label() -> void:
-	round_label.text = "Round   " + str(GameManager.current_round)
-
-func get_unit_info_array(parent_node: Node) -> Array:
-	var unit_info_array = []
-	for unit: Unit in parent_node.get_children():
-		unit_info_array.append(unit.get_info_dict())
-	return unit_info_array
 
 func _on_menu_button_pressed() -> void:
 	%ConfirmReturnMenuPanel.visible = true
@@ -70,3 +71,5 @@ func _on_confirm_return_menu_button_pressed() -> void:
 func _on_return_to_game_button_pressed() -> void:
 	%ConfirmReturnMenuPanel.visible = false
 	%DragDropManager.enable()
+
+#endregion
